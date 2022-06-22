515f3932edd3f45103955a95b2a5b40d73c2aea7
gunvant8008 <gunvant8008@gmail.com>
2022-06-22 14:04:20 +0100

    Final work accordion

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 // Start writing JavaScript here!
+// const menuOpen = document.querySelector('span')
+
+// menuOpen.addEventListener('click' event => {
+//     document.body.classList.add('menu-open')
+// })
+
+const accordion = document.querySelector('.accordion') // do yourself
+accordion.addEventListener('click', event => {
+    const headingsDiv = event.target.closest('.headings'); // value is either null or HTML element
+    if(headingsDiv) {
+        // select closest menu div, then apply class
+        const menu = headingsDiv.closest('.menu')
+        menu.classList.toggle('menu-open')
+    } else {
+        // ... get lost
+    }
+    // 1. can we get the info about the clicked element, target
+    // 2. Check whether the clicked element has any parent with class 'headings'
+    // If so, toggle the open class
+    // Otherwise, ignore the event --- do nothing
+})
+
+// document.body -> shortcut for document.querySelector('menu')
